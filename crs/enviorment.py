1ab5aec58fb94a5386f7e876eeffd6c72890da07
Bazhenov Nikita <140690723+BazhenovNikita@users.noreply.github.com>
2024-07-09 17:11:17 +0500

    Update enviorment.py

--- crs/enviorment.py
+++ crs/enviorment.py
@@ -1,4 +1,3 @@
-ms_token = 'yyrgs2hHE_IHfRrhl3eTJrCLxgV8ZGf2jSlcVjkC4oq3322tcdBe3Q75zkM8GIw1Sm0f3WHBw9umCJj3VWYAIpNklxo1e3AJj51zREwG_gaEsF5vboU-LJT_usGLVgkfKRPrdr4ukq0l7JGhMI8bYsI='
 output_folder = r'../videos'
 ydl_opts = {
     'outtmpl': f'{output_folder}/%(id)s.%(ext)s',
